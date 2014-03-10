1f93c35bd22296ba22dbc73ed2264167e51fbfa9
rampant <rampant1018@gmail.com>
2014-03-10 21:20:18 +0800

    Fix unit-test make rule.

--- mk/test.mk
+++ mk/test.mk
@@ -1,40 +1,41 @@
 QEMU_STM32 ?= ../qemu_stm32/arm-softmmu/qemu-system-arm
 
 check: $(TESTDIR)/unit_test.c $(TESTDIR)/unit_test.h
-	$(MAKE) DEBUG_FLAGS=-DDEBUG
+	$(MAKE) all DEBUG_FLAGS=-DDEBUG
 	$(QEMU_STM32) -nographic -M stm32-p103 \
 		-gdb tcp::3333 -S \
 		-serial stdio \
 		-kernel $(OUTDIR)/$(TARGET).bin -monitor null >/dev/null &
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-strlen.in
-	@mv -f gdb.txt $(TESTDIR)test-strlen.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-strlen.in
+	@mv -f gdb.txt $(TESTDIR)/test-strlen.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-strcpy.in
-	@mv -f gdb.txt $(TESTDIR)test-strcpy.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-strcpy.in
+	@mv -f gdb.txt $(TESTDIR)/test-strcpy.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-strcmp.in
-	@mv -f gdb.txt $(TESTDIR)test-strcmp.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-strcmp.in
+	@mv -f gdb.txt $(TESTDIR)/test-strcmp.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-strncmp.in
-	@mv -f gdb.txt $(TESTDIR)test-strncmp.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-strncmp.in
+	@mv -f gdb.txt $(TESTDIR)/test-strncmp.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-cmdtok.in
-	@mv -f gdb.txt $(TESTDIR)test-cmdtok.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-cmdtok.in
+	@mv -f gdb.txt $(TESTDIR)/test-cmdtok.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-itoa.in
-	@mv -f gdb.txt $(TESTDIR)test-itoa.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-itoa.in
+	@mv -f gdb.txt $(TESTDIR)/test-itoa.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-find_events.in
-	@mv -f gdb.txt $(TESTDIR)test-find_events.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-find_events.in
+	@mv -f gdb.txt $(TESTDIR)/test-find_events.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-find_envvar.in
-	@mv -f gdb.txt $(TESTDIR)test-find_envvar.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-find_envvar.in
+	@mv -f gdb.txt $(TESTDIR)/test-find_envvar.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-fill_arg.in
-	@mv -f gdb.txt $(TESTDIR)test-fill_arg.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-fill_arg.in
+	@mv -f gdb.txt $(TESTDIR)/test-fill_arg.txt
 	@echo
-	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)test-export_envvar.in
-	@mv -f gdb.txt $(TESTDIR)test-export_envvar.txt
+	$(CROSS_COMPILE)gdb -batch -x $(TESTDIR)/test-export_envvar.in
+	@mv -f gdb.txt $(TESTDIR)/test-export_envvar.txt
 	@echo
 	@pkill -9 $(notdir $(QEMU_STM32))
+	@rm -f gdb.txt
