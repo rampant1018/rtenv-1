478aa38c66a954b54a68862a8e4468558e2f6ae5
rampant <rampant1018@gmail.com>
2014-03-11 14:01:53 +0800

    Simplify build target by GNU make macro.

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -1,45 +1,46 @@
 QEMU_STM32 ?= ../qemu_stm32/arm-softmmu/qemu-system-arm
+BUILD_TARGET = $(OUTDIR)/$(TARGET)
 
-qemu: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
-	$(QEMU_STM32) -M stm32-p103 -kernel $(OUTDIR)/$(TARGET).bin
+qemu: $(BUILD_TARGET).bin $(QEMU_STM32)
+	$(QEMU_STM32) -M stm32-p103 -kernel $(BUILD_TARGET).bin
 
-qemudbg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemudbg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin
+		-kernel $(BUILD_TARGET).bin
 
 
-qemu_remote: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
-	$(QEMU_STM32) -M stm32-p103 -kernel $(OUTDIR)/$(TARGET).bin -vnc :1
+qemu_remote: $(BUILD_TARGET).bin $(QEMU_STM32)
+	$(QEMU_STM32) -M stm32-p103 -kernel $(BUILD_TARGET).bin -vnc :1
 
-qemudbg_remote: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemudbg_remote: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin \
+		-kernel $(BUILD_TARGET).bin \
 		-vnc :1
 
-qemu_remote_bg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemu_remote_bg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
-		-kernel $(OUTDIR)/$(TARGET).bin \
+		-kernel $(BUILD_TARGET).bin \
 		-vnc :1 &
 
-qemudbg_remote_bg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
+qemudbg_remote_bg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin \
+		-kernel $(BUILD_TARGET).bin \
 		-vnc :1 &
 
-emu: $(OUTDIR)/$(TARGET).bin
-	bash $(TOOLDIR)/emulate.sh $(OUTDIR)/$(TARGET).bin
+emu: $(BUILD_TARGET).bin
+	bash $(TOOLDIR)/emulate.sh $(BUILD_TARGET).bin
 
-qemuauto: $(OUTDIR)/$(TARGET).bin $(TOOLDIR)/gdbscript
-	bash $(TOOLDIR)/emulate.sh $(OUTDIR)/$(TARGET).bin &
+qemuauto: $(BUILD_TARGET).bin $(TOOLDIR)/gdbscript
+	bash $(TOOLDIR)/emulate.sh $(BUILD_TARGET).bin &
 	sleep 1
 	$(CROSS_COMPILE)gdb -x $(TOOLDIR)/gdbscript&
 	sleep 5
 
-qemuauto_remote: $(OUTDIR)/$(TARGET).bin $(TOOLDIR)/gdbscript
-	bash $(TOOLDIR)/emulate_remote.sh $(OUTDIR)/$(TARGET).bin &
+qemuauto_remote: $(BUILD_TARGET).bin $(TOOLDIR)/gdbscript
+	bash $(TOOLDIR)/emulate_remote.sh $(BUILD_TARGET).bin &
 	sleep 1
 	$(CROSS_COMPILE)gdb -x $(TOOLDIR)/gdbscript&
 	sleep 5
